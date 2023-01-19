dba2a827885835f1ed60bc9e53b30b176fa2eaf2
0svaldo-Chavz <os_antony99@hotmail.com>
2023-01-18 18:02:07 -0600

    cAMBIOS

--- api/src/routes/carta.routes.js
+++ api/src/routes/carta.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as cartaCtrl from "../controllers/carta.controller.js";
+import { filesUpload } from "../utils/cloudinary.js";
 
 const router = Router();
 
 router.get("/", cartaCtrl.getCartas);
 router.get("/buscar/:idCasa", cartaCtrl.getCartaById);
-router.post("/nuevaCarta", cartaCtrl.crearCarta);
+router.post("/nuevaCarta", filesUpload(), cartaCtrl.crearCarta);
 router.delete("/eliminarCarta/:idCasa", cartaCtrl.eliminarCarta);
 
 export default router;
