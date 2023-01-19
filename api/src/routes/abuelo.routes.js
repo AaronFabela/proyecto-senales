ab4a5dc22dd3619bd57260c4c15d084e55ad7d69
Aaron Fabela <aaron1998.jfg@gmail.com>
2023-01-18 19:05:20 -0600

    final

--- api/src/routes/abuelo.routes.js
+++ api/src/routes/abuelo.routes.js
@@ -1,15 +1,15 @@
-import { Router } from "express";
-import * as abueloCtrl from "../controllers/abuelo.controller.js";
-import { filesUpload } from "../utils/cloudinary.js";
+import { Router } from 'express'
+import * as abueloCtrl from '../controllers/abuelo.controller.js'
+import { filesUpload } from '../utils/cloudinary.js'
 
-const router = Router();
+const router = Router()
 
-router.get("/", abueloCtrl.getAbuelos);
-router.get("/buscar/:idAbuelo", abueloCtrl.getAbueloById);
-router.get("/buscarPorCasa/:idCasa", abueloCtrl.getAbueloById);
-router.get("/buscarPorAdoptador/:idAdoptador", abueloCtrl.getAbueloById);
-router.put("/leerCarta/idCarta", abueloCtrl.leerCarta);
-router.post("/registrar", filesUpload(), abueloCtrl.crearAbuelo);
-router.delete("/eliminarRegistro/:idAbuelo", abueloCtrl.eliminarAbuelo);
+router.get('/', abueloCtrl.getAbuelos)
+router.get('/buscar/:idAbuelo', abueloCtrl.getAbueloById)
+router.get('/buscarPorCasa/:idCasa', abueloCtrl.getAbueloById)
+router.get('/buscarPorAdoptador/:idAdoptador', abueloCtrl.getAbueloById)
+router.put('/leerCarta/:idCarta', abueloCtrl.leerCarta)
+router.post('/registrar', filesUpload(), abueloCtrl.crearAbuelo)
+router.delete('/eliminarRegistro/:idAbuelo', abueloCtrl.eliminarAbuelo)
 
-export default router;
+export default router
--- api/src/routes/carta.routes.js
+++ api/src/routes/carta.routes.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import * as cartaCtrl from "../controllers/carta.controller.js";
-import { filesUpload } from "../utils/cloudinary.js";
+import { Router } from 'express'
+import * as cartaCtrl from '../controllers/carta.controller.js'
+import { filesUpload } from '../utils/cloudinary.js'
 
-const router = Router();
+const router = Router()
 
-router.get("/", cartaCtrl.getCartas);
-router.get("/buscar/:idCasa", cartaCtrl.getCartaById);
-router.post("/nuevaCarta", filesUpload(), cartaCtrl.crearCarta);
-router.delete("/eliminarCarta/:idCasa", cartaCtrl.eliminarCarta);
+router.get('/', cartaCtrl.getCartas)
+router.get('/buscar/:idCarta', cartaCtrl.getCartaById)
+router.post('/nuevaCarta', filesUpload(), cartaCtrl.crearCarta)
+router.delete('/eliminarCarta/:idCarta', cartaCtrl.eliminarCarta)
 
-export default router;
+export default router
